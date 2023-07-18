1e48f81c180ba722f0f4ae7b61bd5ff60a3b72ab
Jazmin-Barron-Cuenca <1930280@upt.edu.mx>
2023-07-17 18:05:50 -0600

    Vistas en desarrollo

---
+++ app/src/main/java/com/example/cafeteria_pos/cocina/CocinaActivity.java
@@ -0,0 +1,16 @@
+package com.example.cafeteria_pos.cocina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cafeteria_pos.R;
+
+public class CocinaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cocina);
+    }
+}
---
+++ app/src/main/java/com/example/cafeteria_pos/pos/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.cafeteria_pos.pos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cafeteria_pos.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posmain);
+    }
+}
